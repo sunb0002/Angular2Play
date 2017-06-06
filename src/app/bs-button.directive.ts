69c46bb3b2f68845d80342eee8b5b07ca145f9ab
sun.bo <st_sunbo@hotmail.com>
2017-06-06 16:36:08 +0800

    Directive event listen by html/ by HostListener

--- src/app/bs-button.directive.ts
+++ src/app/bs-button.directive.ts
@@ -1,28 +1,44 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
-  selector: 'button[appBsButton]'
+  selector: 'button[appBsButton], span[appBsButtonSpan]'
 })
 export class BsButtonDirective {
 
   @Input() appBsButton;
 
+  defaultButton: string = 'primary';
+  overButton: string = 'danger';
+
+  showMsg: boolean = true;
+
   constructor(private el: ElementRef) { }
 
   ngOnInit() {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     let testInput = this.appBsButton;
 
     //// ES6/JSX Template literals
-    console.log("BsButtonDirective testInput: ${testInput}");
-
+    console.log(`BsButtonDirective testInput: ${testInput}`);
 
     const button = (this.el.nativeElement as HTMLElement);
-    button.classList.add('btn');
-    //// Js/Ts hack way to set default value, using ||
-    button.classList.add(`btn-${testInput || 'primary'}`);
+    // HTML element.tagName
+    console.log("This is a %o element. Tagname=" + button.tagName, button);
 
+    if (button.tagName.toUpperCase() === "BUTTON") {
+      button.classList.add('btn');
+      //// Js/Ts hack way to set default value, using ||
+      button.classList.add(`btn-${testInput || this.defaultButton}`);
+
+    }
+  }
+
+  @HostListener('mouseover')
+  doMouseOver() {
+    console.log(`BsButtonDirective testInput: ${this.appBsButton}`);
+    const buttonX = (this.el.nativeElement as HTMLElement);
+    if (buttonX.tagName.toUpperCase() === "SPAN") {
+      buttonX.classList.toggle('bg-success');
+    }
   }
 
 }

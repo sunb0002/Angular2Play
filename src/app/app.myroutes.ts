fce543c0b91d4f281521041f6d4f695b448d174d
sun.bo <st_sunbo@hotmail.com>
2017-06-07 20:20:27 +0800

    Router minor change before AuthGuard大魔王

--- src/app/app.myroutes.ts
+++ src/app/app.myroutes.ts
@@ -5,9 +5,9 @@ import { Persona1Component } from './sbroutes/persona1/persona1.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'per1', component: Persona1Component },
+    { path: 'perX', component: PersonaNotFoundComponent },
     { path: 'per2', component: Persona2Component },
     { path: 'per3', redirectTo: 'per1' },
     { path: 'persub', loadChildren: './sbroutes/router2/router2.module#Router2Module' },
-    { path: '**', component: PersonaNotFoundComponent }
+    { path: '**', component: Persona1Component }
 ];

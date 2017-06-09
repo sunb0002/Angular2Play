6f8190df7199be2e2b1340f06fdd75c14461b968
sun.bo <st_sunbo@hotmail.com>
2017-06-09 19:28:45 +0800

    Reactive Form Validation

--- src/app/sbroutes/router2/pripara/pripara.component.ts
+++ src/app/sbroutes/router2/pripara/pripara.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-pripara',
   templateUrl: './pripara.component.html',
   styleUrls: ['./pripara.component.css']
 })
-export class PriparaComponent implements OnInit {
+export class PriparaComponent {
 
-  constructor() { }
+  sbFG = new FormGroup({});
 
-  ngOnInit() {
+  formErrors = {
+    name: '',
+    email: ''
+  };
+
+  // Can't change the name minlength,maxlength,email
+  // Since the names are from FormControl.error (Validator names)
+  validationMsg = {
+    name: {
+      required: 'Name: no empty',
+      minlength: 'Name: at least 4 chars',
+      maxlength: 'Name: at most 8 chars'
+    },
+    email: {
+      required: 'Email: no empty',
+      email: 'Email: wrong format'
+    }
+  };
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    let nameCtrl = new FormControl('sb', [Validators.required, Validators.maxLength(8), Validators.minLength(4)]);
+    let emailCtrl = new FormControl('', [Validators.required, Validators.email]);
+
+    this.sbFG.addControl('name', nameCtrl); // My way is better than offical docs
+    this.sbFG.addControl('email', emailCtrl);
+
+    console.log('sbFG FormGroup: ', this.sbFG);
+    this.sbFG.valueChanges.subscribe(data => this.checkForm(data)); //onValueChanged
+    this.checkForm(); // (re)set validation messages now
+  }
+
+  checkForm(data?: any) {
+    if (!this.sbFG) {
+      return;
+    }
+
+    const form = this.sbFG;
+    for (let field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+
+      // check for new error message
+      let ct = form.get(field);
+      if (ct && ct.dirty && !ct.valid) {
+        const msg = this.validationMsg[field];
+        //console.log('msg: ', msg);
+        //console.log('ct.errors', ct.errors);
+
+        for (const key in ct.errors) {
+          this.formErrors[field] += msg[key] + ' ';
+        }
+      }
+
+    }
+  }
+
+  doSubmit() {
+    confirm('Here we go!');
   }
 
 }
+
+
+
--- src/app/sbroutes/router2/router2.module.ts
+++ src/app/sbroutes/router2/router2.module.ts
@@ -6,11 +6,13 @@ import { PrecureComponent } from './precure/precure.component';
 import { UltramanComponent } from './ultraman/ultraman.component';
 import { PrecureBaseComponent } from './precure-base/precure-base.component';
 import { PriparaComponent } from './pripara/pripara.component';
+import { ReactiveFormsModule } from "@angular/forms"; //Must add this manually to support reactive form
 
 @NgModule({
   imports: [
     CommonModule,
-    Router2RoutingModule
+    Router2RoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [PrecureComponent, UltramanComponent, PrecureBaseComponent, PriparaComponent]
 })

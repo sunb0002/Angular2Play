5bb7cef595eb6729af132cb3668b3af38d2e3a58
sun.bo <st_sunbo@hotmail.com>
2017-07-16 10:10:06 +0800

    jQuery integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 
 import { SbhttpService } from './sbhttp.service';
 import { SbstatusService } from './sbstatus.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app works!';
   //todoItems: TodoItem[];
 
@@ -24,6 +26,38 @@ export class AppComponent {
         this.s2.setLoading(false);
       }
     );
+  }
+
+  ngAfterViewInit() {
+
+    // const $ = jQuery;
+
+    $(document).ready(function () {
+      $('.menu-link').bigSlide({
+        menu: '#menu',
+        side: 'right',
+        easyClose: 'true'
+      });
+      console.log('bigslide');
+
+      $('#search-button').on('click', function (e) {
+        if ($('#search-input-container').hasClass('hdn')) {
+          e.preventDefault();
+          $('#search-input-container').removeClass('hdn')
+          return false;
+        }
+        console.log('search click');
+      });
+
+      $('#hide-search-input-container').on('click', function (e) {
+        e.preventDefault();
+        $('#search-input-container').addClass('hdn')
+        console.log('search click222');
+        return false;
+      });
+    });
+
+
   }
 
 
